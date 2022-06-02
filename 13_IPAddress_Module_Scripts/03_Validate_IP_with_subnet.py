40dfdc8da70763efea1737f9dd4241b687e64c19
NetworkEvolution <70020386+network-evolution@users.noreply.github.com>
2022-06-02 08:37:30 +0530

    IP Subnet validator Script Update

---
+++ 13_IPAddress_Module_Scripts/03_Validate_IP_with_subnet.py
@@ -0,0 +1,27 @@
+#! /usr/local/Python_envs/Python3/bin/python3
+import ipaddress
+from colorama import Fore,Style
+ip_list = []
+while True:
+	try:
+		ip_input = input(f"\n\n{'~'*50}\nEnter IP Address/list/exit: ")
+		if ip_input == 'exit':
+			print("Exiting the script")
+			exit()
+		if ip_input == 'list':
+			print(f"Current Assigned List is:{sorted(ip_list)}")
+			continue
+		else:
+			ip_input = ipaddress.ip_address((ip_input))
+		lan_subnet = ipaddress.ip_network("192.168.0.0/24")
+
+		if ip_input not in ip_list:
+			if ip_input in lan_subnet:
+				ip_list.append(ip_input)
+				print(f"{Fore.GREEN}{Style.BRIGHT}Value Accepted:  IP {ip_input}{Style.RESET_ALL} is added to the list ")
+			else:
+				print(f"{Fore.YELLOW}{Style.BRIGHT}Subnet mismatch.{Style.NORMAL} Enter IP in {Fore.CYAN}192.168.0.0/24 {Fore.YELLOW}Range{Style.RESET_ALL}")
+		else:
+			print("IP already exist")
+	except ValueError:
+		print(f"{Fore.RED}{Style.BRIGHT}Invalid IP{Style.RESET_ALL}")
