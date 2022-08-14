ded9af71db2c149b31746435acad3439b8cc4ab4
NetworkEvolution <70020386+network-evolution@users.noreply.github.com>
2022-08-14 14:50:44 +0530

    Create 03_nxapi_json_rpc_exceptions.py

---
+++ 17_NXOS-API_Automation/03_nxapi_json_rpc_exceptions.py
@@ -0,0 +1,76 @@
+#! /usr/local/Python_envs/Python3/bin/python3
+import requests
+import json
+from pprint import pprint
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+switchuser='admin'
+switchpassword='admin'
+url='https://192.168.0.201/ins'
+myheaders={'content-type':'application/json-rpc'}
+
+payload=[
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "interface e1/2",
+      "version": 1
+    },
+    "id": 1,
+    "rollback": "rollback-on-error"
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "description from-jsonrpc",
+      "version": 1
+    },
+    "id": 2,
+    "rollback": "rollback-on-error"
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "aaa",
+      "version": 1
+    },
+    "id": 3,
+    "rollback": "rollback-on-error"
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "no switchport",
+      "version": 1
+    },
+    "id": 4,
+    "rollback": "rollback-on-error"
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "ip add 1.1.1.1 255.255.255.0",
+      "version": 1
+    },
+    "id": 5,
+    "rollback": "rollback-on-error"
+  }
+]
+
+
+try:
+  response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword), verify=False)
+  print(response.status_code)
+  if response.status_code != 200:
+    raise Exception (f"{response.status_code} \n {response.text}")
+  pprint(response.json())
+
+except Exception as e:
+  print(f"Exception occurred with: {e}")
